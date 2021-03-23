3ad7183e9b475a8ab7080a08abc6deef03146fe7
Gabe Sawhney <gabe@pwd.ca>
2021-03-23 19:43:02 -0400

    add mysql schema file

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `searches` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `lastran` datetime DEFAULT NULL,
+  `searchphrase` varchar(255) DEFAULT NULL,
+  `emailvalidated` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8
+
+CREATE TABLE `notifications` (
+  `id` mediumint(9) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `reference` varchar(255) NOT NULL,
+  `meetingdate` varchar(255) DEFAULT NULL,
+  `decisionBodyName` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `emailsent` char(1) DEFAULT '0',
+  PRIMARY KEY (`id`,`reference`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
